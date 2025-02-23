4b3cea68724837e4e49cd294d73a86993f4d9d42
humang <43851194+humang@users.noreply.github.com>
2025-02-23 10:05:56 +0900

    "feat | 컨트롤러 | 컨트롤러 설명추가 | 황민우 "

--- src/main/java/recipe/controller/ReportController.java
+++ src/main/java/recipe/controller/ReportController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import recipe.entity.Report;
 import recipe.entity.Report.ReportStatus;
 import recipe.entity.User;
@@ -30,6 +31,7 @@ public class ReportController {
 
 	//레시피 신고
     @PostMapping("/{recipeId}")
+    @Operation(summary = "레시피 신고", description = "일반 사용자의 레시피 신고 기능")
     public ResponseEntity<String> reportPost(@PathVariable Long recipeId, @RequestParam String reason) {
         reportService.reportRecipe(recipeId, reason);
         return ResponseEntity.ok("신고가 접수되었습니다.");
@@ -37,6 +39,7 @@ public class ReportController {
     
     // 관리자의 신고 목록 조회
     @GetMapping
+    @Operation(summary = "신고목록 조회", description = "관리자의 신고 목록 조회 기능")
     public ResponseEntity<List<Report>> getReportsForAdmin(@RequestBody User admin) {
         List<Report> reports = adminService.getAllPendingReports(admin);
         return ResponseEntity.ok(reports);
@@ -44,6 +47,7 @@ public class ReportController {
 
     // 관리자의 신고 처리
     @PostMapping("/{reportId}")
+    @Operation(summary = "신고 처리 기능", description = "관리자의 신고 처리 기능")
     public ResponseEntity<String> resolveReport(@RequestBody User admin, @PathVariable Long reportId, @RequestParam ReportStatus status) {
         adminService.updateReportStatus(admin, reportId, status);
         return ResponseEntity.ok("신고가 처리되었습니다.");
